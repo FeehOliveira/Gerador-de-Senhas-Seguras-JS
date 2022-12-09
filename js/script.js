f9fa6d193f8b1ce53849e36d9e126c849e738044
FeehOliveira <feeholiveira10@outlook.com>
2022-12-09 16:17:13 -0300

    upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("teste")
+//Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generated-password");
+
+//Funções
+
+
+
+//Eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste")
+})
